3269b58607ff42e1361b54989c7f7db79af843ab
Sadek-1801 <hmashrafujjaman@gmail.com>
2024-05-13 03:34:23 +0600

    post data applied

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
 
 // middleware
-require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qv5d3vd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const jobsCollection = client.db('nexPath').collection('allJobs')
+
+    app.get('/jobs', async(req, res) => {
+        const result = await jobsCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.post('/job', async(req, res) => {
+        const jobData = req.body 
+        const result = await jobsCollection.insertOne(jobData) 
+        res.send(result)
+    })
+
+    app.get('/job/:id', async(req, res) => {
+      const id = req.params.id;
+      const result = await jobsCollection.findOne({_id: new ObjectId(id)})
+      res.send(result)
+    })
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send('nexpath server is running')
-})
+  res.send("nexpath server is running");
+});
 app.listen(port, () => {
-    console.log(`nexpath server is running on ${port}`)
-})
+  console.log(`nexpath server is running on ${port}`);
+});

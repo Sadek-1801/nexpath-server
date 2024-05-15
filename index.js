cfe60fee67108ed96a839b06bb46f3501dc6f50a
Sadek-1801 <hmashrafujjaman@gmail.com>
2024-05-15 23:53:55 +0600

    finishing just for correct functionality

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://nex-path.web.app", "https://nexpath.netlify.app"],
     credentials: true,
     optionSuccessStatus: 200,
   })
